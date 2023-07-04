f80e0747ade2db291a72d416de0583beb70eebf7
Rafiyan06 <Rafiyanuar060401@gmail.com>
2023-07-04 23:56:07 +0700

    first commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use Orm\Post;
+
+class Welcome extends CI_Controller {
+	public function index()
+	{
+		$post_list = Post::all()->sortByDesc('created_at');
+		view('post', ['post_list' => $post_list ]);
+	}
+}
